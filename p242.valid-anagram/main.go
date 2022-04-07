8fe5c03f1704f2619df9057c35c24dd7fde786ae
leopoldxx <leopoldxx@gmail.com>
2022-04-07 21:38:32 +0800

    p242. https://leetcode.com/problems/valid-anagram/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p242.valid-anagram/main.go
+++ p242.valid-anagram/main.go
@@ -4,18 +4,17 @@ func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	cache := map[byte]int{}
+	cache := [26]int{}
 	bs := []byte(s)
 	bt := []byte(t)
 	for i := 0; i < len(bs); i++ {
-		cache[bs[i]]++
-		cache[bt[i]]--
+		cache[int(bs[i]-'a')]++
+		cache[int(bt[i]-'a')]--
 	}
 	for _, v := range cache {
 		if v != 0 {
 			return false
 		}
-
 	}
 	return true
 }
