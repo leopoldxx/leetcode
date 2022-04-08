bfc0acb281fcb7ebba18747ec5c44b5d1803cd9e
leopoldxx <leopoldxx@gmail.com>
2022-04-08 23:52:15 +0800

    p34. https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p34.find-first-and-last-position-of-element-in-sorted-array/main.go
+++ p34.find-first-and-last-position-of-element-in-sorted-array/main.go
@@ -1,34 +1,39 @@
 package main
 
 func findLeft(nums []int, target int) int {
+	idx := -1
 	left, right := 0, len(nums)-1
 	for {
-		if left >= right {
-			return right
+		if left > right {
+			return idx
 		}
 		mid := left + (right-left)/2
 		if nums[mid] >= target {
-			right = mid
+			right = mid - 1
 		} else {
 			left = mid + 1
+		}
+		if nums[mid] == target {
+			idx = mid
 		}
 	}
 }
 
 func findRight(nums []int, target int) int {
+	idx := -1
 	left, right := 0, len(nums)-1
 	for {
-		if left >= right {
-			return right
+		if left > right {
+			return idx
 		}
 		mid := left + (right-left)/2
-		if right-left == 1 {
-			mid += 1
-		}
 		if nums[mid] <= target {
-			left = mid
+			left = mid + 1
 		} else {
 			right = mid - 1
+		}
+		if nums[mid] == target {
+			idx = mid
 		}
 	}
 }
