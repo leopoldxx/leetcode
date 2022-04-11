34b6021016ad6125b71d34dadbb6c9c2bbeef2ac
leopoldxx <leopoldxx@gmail.com>
2022-04-12 00:13:28 +0800

    p918. https://leetcode.com/problems/maximum-sum-circular-subarray/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p918.maximum-sum-circular-subarray/main.go
+++ p918.maximum-sum-circular-subarray/main.go
@@ -1,25 +1,31 @@
 package main
 
-func max(a, b int) int {
-	if a < 0 {
+func max2(a, b int) int {
+	if a < b {
 		return b
 	}
 	return a
 }
-
-func min(a, b int) int {
-	if a > 0 {
-		return b
+func max3(a, b, c int) int {
+	if a < 0 {
+		return c
 	}
-	return a
+	return b
+}
+
+func min3(a, b, c int) int {
+	if a > 0 {
+		return c
+	}
+	return b
 }
 
 func maxSubarraySumCircular(nums []int) int {
 	total, maxSum, minSum, currentMaxSum, currentMinSum := nums[0], nums[0], nums[0], nums[0], nums[0]
 	for i := 1; i < len(nums); i++ {
 		total += nums[i]
-		currentMaxSum = max(currentMaxSum+nums[i], nums[i])
-		currentMinSum = min(currentMinSum+nums[i], nums[i])
+		currentMaxSum = max3(currentMaxSum, currentMaxSum+nums[i], nums[i])
+		currentMinSum = min3(currentMinSum, currentMinSum+nums[i], nums[i])
 		if maxSum < currentMaxSum {
 			maxSum = currentMaxSum
 		}
@@ -27,7 +33,13 @@ func maxSubarraySumCircular(nums []int) int {
 			minSum = currentMinSum
 		}
 	}
+	if minSum > 0 {
+		return total
+	}
+	if maxSum < 0 {
+		return maxSum
+	}
 
-	return max(maxSum, total+minSum)
+	return max2(maxSum, total-minSum)
 
 }
