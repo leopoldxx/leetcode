4e8840f6cd4eca4fe3edfc0c14aab41872be1bd9
leopoldxx <leopoldxx@gmail.com>
2022-04-14 23:30:54 +0800

    p144. https://leetcode.com/problems/binary-tree-preorder-traversal/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

---
+++ p144.binary-tree-preorder-traversal/main.go
@@ -0,0 +1,14 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	return append(append([]int{root.Val}, preorderTraversal(root.Left)...), preorderTraversal(root.Right)...)
+}
