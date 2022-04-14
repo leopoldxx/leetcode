33be1a6ff3f538ba441379fbd2410f1e2098d524
leopoldxx <leopoldxx@gmail.com>
2022-04-14 23:35:07 +0800

    p145. https://leetcode.com/problems/binary-tree-postorder-traversal/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

---
+++ p145.binary-tree-postorder-traversal/main.go
@@ -0,0 +1,14 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	return append(append(postorderTraversal(root.Left), postorderTraversal(root.Right)...), root.Val)
+}
