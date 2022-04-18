324bfe63dd9650d6ac7fef2a7bd4018b091cb890
leopoldxx <leopoldxx@gmail.com>
2022-04-19 00:45:49 +0800

    p206. https://leetcode.com/problems/palindrome-linked-list/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p206.reverse-linked-list/main.go
+++ p206.reverse-linked-list/main.go
@@ -5,16 +5,50 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func reverseList(head *ListNode) *ListNode {
-	var (
-		pre     *ListNode
-		newHead *ListNode
-	)
+func reverse(head *ListNode) (newHead *ListNode, tail *ListNode) {
+	tail = head
+	var pre *ListNode
+
 	for head != nil {
 		pre = newHead
 		newHead = head
 		head = head.Next
+
 		newHead.Next = pre
 	}
-	return newHead
+	return newHead, tail
+
+}
+func equal(l, r *ListNode) bool {
+	for {
+		if l == r {
+			return true
+		}
+		if l.Val != r.Val {
+			return false
+		}
+		l = l.Next
+		r = r.Next
+	}
+
+}
+func isPalindrome(head *ListNode) bool {
+	if head.Next == nil {
+		return true
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		if fast != nil {
+			slow = slow.Next
+		}
+	}
+	l2, t2 := reverse(slow.Next)
+
+	if fast != nil {
+		t2.Next = slow
+	}
+	slow.Next = nil
+
+	return equal(head, l2)
 }
--- p206.reverse-linked-list/main_test.go
+++ p206.reverse-linked-list/main_test.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSolution(t *testing.T) {
-	reverseList(nil)
+	assert.True(t, isPalindrome(&ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 1,
+				},
+			},
+		},
+	}))
+	assert.True(t, isPalindrome(&ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 1,
+			},
+		},
+	}))
+	assert.False(t, isPalindrome(&ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+		},
+	}))
 }
