1febff43c1aeb2adaf4c5777f30349314d0b0008
leopoldxx <leopoldxx@gmail.com>
2022-04-22 16:34:49 +0800

    p11. https://leetcode.com/problems/container-with-most-water/submissions/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p11.container-with-most-water/main.go
+++ p11.container-with-most-water/main.go
@@ -1,18 +1,20 @@
 package main
 
-func maxArea(height []int) int {
-	area, left, right := 0, 0, len(height)-1
-	max, min := func(l, r int) int {
-		if l < r {
-			return r
-		}
-		return l
-	}, func(l, r int) int {
-		if l < r {
-			return l
-		}
+func max(l, r int) int {
+	if l < r {
 		return r
 	}
+	return l
+}
+func min(l, r int) int {
+	if l < r {
+		return l
+	}
+	return r
+}
+func maxArea(height []int) int {
+	area, left, right := 0, 0, len(height)-1
+
 	for left < right {
 		area = max(area, (right-left)*(min(height[left], height[right])))
 		if height[left] < height[right] {
