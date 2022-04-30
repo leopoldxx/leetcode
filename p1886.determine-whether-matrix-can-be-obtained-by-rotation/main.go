a63ed3e409a672a01be81bf621afb001f9b6f84a
leopoldxx <leopoldxx@gmail.com>
2022-04-30 10:28:09 +0800

    p1886. https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

---
+++ p1886.determine-whether-matrix-can-be-obtained-by-rotation/main.go
@@ -0,0 +1,22 @@
+package main
+
+func equalRotate(mat [][]int, target [][]int) bool {
+	equalRotate0 := true
+	equalRotate90 := true
+	equalRotate180 := true
+	equalRotate270 := true
+	length := len(mat)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			equalRotate0 = equalRotate0 && mat[i][j] == target[i][j]
+			equalRotate90 = equalRotate90 && mat[i][j] == target[j][length-1-i]
+			equalRotate180 = equalRotate180 && mat[i][j] == target[length-i-1][length-j-1]
+			equalRotate270 = equalRotate270 && mat[i][j] != target[length-j-1][i]
+		}
+	}
+	return true
+}
+
+func findRotation(mat [][]int, target [][]int) bool {
+	return equalRotate(mat, target)
+}
