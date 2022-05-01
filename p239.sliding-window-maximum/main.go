e1feb2c58e8e8bbafefaf9d8b923f6352ffbf935
leopoldxx <leopoldxx@gmail.com>
2022-05-01 19:39:43 +0800

    p239. https://leetcode.com/problems/sliding-window-maximum/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

---
+++ p239.sliding-window-maximum/main.go
@@ -0,0 +1,135 @@
+package main
+
+type item struct {
+	val     int
+	idx     int
+	heapIdx int
+}
+
+type maxheap struct {
+	indice map[int]*item
+	data   []*item
+}
+
+func (h *maxheap) init() {
+	h.indice = make(map[int]*item)
+}
+func (h *maxheap) last() int {
+	return len(h.data) - 1
+}
+
+func (h *maxheap) siftup(idx int) {
+	for {
+		p := (idx - 1) / 2
+		if p == idx { // idx == 0
+			return
+		}
+		if h.data[idx].val < h.data[p].val {
+			return
+		}
+		h.data[idx], h.data[p] = h.data[p], h.data[idx]
+		h.data[idx].heapIdx = idx
+		h.data[p].heapIdx = p
+		idx = p
+	}
+}
+func (h *maxheap) siftdown(idx int) (changed bool) {
+	for {
+		left := idx*2 + 1
+		if left >= len(h.data) {
+			return
+		}
+		cand := left
+		if right := left + 1; right < len(h.data) && h.data[right].val > h.data[left].val {
+			cand = right
+		}
+
+		if h.data[cand].val <= h.data[idx].val {
+			return
+		}
+		changed = true
+		h.data[cand], h.data[idx] = h.data[idx], h.data[cand]
+		h.data[cand].heapIdx = cand
+		h.data[idx].heapIdx = idx
+		idx = cand
+	}
+}
+func (h *maxheap) push(v item) {
+	h.indice[v.idx] = &v
+	h.data = append(h.data, &v)
+	v.heapIdx = h.last()
+	h.siftup(h.last())
+}
+
+func (h *maxheap) top() *item {
+	if len(h.data) == 0 {
+		return nil
+	}
+	return h.data[0]
+}
+
+func (h *maxheap) remove(idx int) {
+	it, ok := h.indice[idx]
+	if !ok {
+		return
+	}
+
+	heapIdx := it.heapIdx
+	if heapIdx == h.last() {
+		h.data[h.last()].heapIdx = -1
+		h.data = h.data[:h.last()]
+		return
+	}
+
+	h.data[heapIdx], h.data[h.last()] = h.data[h.last()], h.data[heapIdx]
+	h.data[heapIdx].heapIdx = heapIdx
+	h.data[h.last()].heapIdx = -1
+	h.data = h.data[:h.last()]
+
+	delete(h.indice, idx)
+
+	h.siftdown(heapIdx)
+	h.siftup(heapIdx)
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	if len(nums) == 0 {
+		return nil
+	} else if len(nums) < k {
+		return nil
+	}
+
+	maxSumSlice := make([]int, 0, len(nums))
+	heap := &maxheap{}
+	heap.init()
+
+	for i := 0; i < len(nums); i++ {
+		if i < k-1 {
+			heap.push(item{val: nums[i], idx: i})
+		} else {
+			heap.push(item{val: nums[i], idx: i})
+			maxSumSlice = append(maxSumSlice, heap.top().val)
+			heap.remove(i - k + 1)
+		}
+
+	}
+	return maxSumSlice
+}
+
+func findMax(nums []int) int {
+	maxNum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if maxNum < nums[i] {
+			maxNum = nums[i]
+		}
+	}
+	return maxNum
+}
+
+func maxSlidingWindowBrute(nums []int, k int) []int {
+	maxSumSlice := make([]int, 0, len(nums))
+	for i := 0; i < len(nums)-k+1; i++ {
+		maxSumSlice = append(maxSumSlice, findMax(nums[i:i+k]))
+	}
+	return maxSumSlice
+}
