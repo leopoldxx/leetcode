b9cb3fd2174371a3a71da2c76ee6eb5b7912ef38
leopoldxx <leopoldxx@gmail.com>
2022-05-10 22:46:03 +0800

    p1014. https://leetcode.com/problems/best-sightseeing-pair/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p1014.best-sightseeing-pair/main.go
+++ p1014.best-sightseeing-pair/main.go
@@ -1,19 +1,22 @@
 package main
 
 func maxScoreSightseeingPair(values []int) int {
-	maxPre := values[0]
-	maxTotal := values[0]
+	preMaxSpotValue := values[0] + 0
+	maxScore := -1
 
 	for i := 1; i < len(values); i++ {
-		if maxPre+values[i]-i > maxTotal {
-			maxTotal = maxPre + values[i] - i
+		value1 := values[i] + i // act as the first spot
+		value2 := values[i] - i // act as the second spot
+
+		if preMaxSpotValue+value2 > maxScore {
+			maxScore = preMaxSpotValue + value2
 		}
 
-		if values[i]+i > maxPre {
-			maxPre = values[i] + i
+		if value1 > preMaxSpotValue {
+			preMaxSpotValue = value1
 		}
 
 	}
-	return maxTotal
+	return maxScore
 
 }
