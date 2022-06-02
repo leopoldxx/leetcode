a871bf258b56aa419adbda47b5d43221cb1acf84
leopoldxx <leopoldxx@gmail.com>
2022-06-02 21:07:24 +0800

    p27. https://leetcode.com/problems/remove-element/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p27.remove-element/main.go
+++ p27.remove-element/main.go
@@ -1,24 +1,12 @@
 package main
 
 func removeElement(nums []int, val int) int {
-	if len(nums) == 0 {
-		return 0
-	} else if len(nums) == 1 && nums[0] == val {
-		return 0
-	} else if len(nums) == 1 && nums[0] != val {
-		return 1
-	}
-	inplace := nums
 	idx := 0
-	for {
-		if idx == len(inplace) {
-			break
-		}
-		if inplace[idx] == val {
-			inplace[idx] = inplace[len(inplace)-1]
-			inplace = inplace[:len(inplace)-1]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
 			continue
 		}
+		nums[idx] = nums[i]
 		idx++
 	}
 	return idx
