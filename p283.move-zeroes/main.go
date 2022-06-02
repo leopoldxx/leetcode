3bd932580df23e5d11962d19657f97556ee678d5
leopoldxx <leopoldxx@gmail.com>
2022-06-02 21:14:34 +0800

    p283. https://leetcode.com/problems/move-zeroes/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p283.move-zeroes/main.go
+++ p283.move-zeroes/main.go
@@ -1,30 +1,17 @@
 package main
 
 func moveZeroes(nums []int) {
-	if len(nums) == 1 {
-		return
-	}
-	zeroPointer := 0
-	nonZeroPointer := 1
-	for nonZeroPointer < len(nums) && zeroPointer < len(nums) {
-		for zeroPointer < len(nums) && nums[zeroPointer] != 0 {
-			zeroPointer++
-		}
-		if zeroPointer == len(nums) {
-			break
-		}
-		if nonZeroPointer < zeroPointer {
-			nonZeroPointer = zeroPointer + 1
-		}
-		for nonZeroPointer < len(nums) && nums[nonZeroPointer] == 0 {
-			nonZeroPointer++
-		}
-		if nonZeroPointer == len(nums) {
-			break
-		}
-		nums[zeroPointer], nums[nonZeroPointer] = nums[nonZeroPointer], nums[zeroPointer]
-		zeroPointer++
-		nonZeroPointer++
-	}
+	idx := 0
+	target := 0
 
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			continue
+		}
+		nums[idx] = nums[i]
+		idx++
+	}
+	for i := idx; i < len(nums); i++ {
+		nums[i] = target
+	}
 }
