9f97216266e9dcae9e78c90531cf0ad495b4122f
leopoldxx <leopoldxx@gmail.com>
2022-06-06 23:32:42 +0800

    p15. https://leetcode.com/problems/3sum/
    Signed-off-by: leopoldxx <leopoldxx@gmail.com>

--- p15.3sum/main.go
+++ p15.3sum/main.go
@@ -2,43 +2,39 @@ package main
 
 import "sort"
 
-func binarySearch(nums []int, left, right, target int) int {
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if target == nums[mid] {
-			return mid
-		} else if nums[mid] < target {
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return -1
-}
-
 func threeSum(nums []int) [][]int {
 	if len(nums) < 3 {
 		return nil
 	}
-	type triplet [3]int
-	filter := map[triplet]struct{}{}
 
 	sort.Ints(nums)
-	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			complement := -(nums[i] + nums[j])
-			k := binarySearch(nums, j+1, len(nums)-1, complement)
-			if k == -1 {
-				continue
-			}
-			filter[triplet{nums[i], nums[j], nums[k]}] = struct{}{}
-		}
-	}
 
 	var result [][]int
-	for t := range filter {
-		result = append(result, []int{t[0], t[1], t[2]})
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i != 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		sum := -nums[i]
+		left := i + 1
+		right := len(nums) - 1
+		for left < right {
+			if nums[left]+nums[right] == sum {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				left++
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				right--
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else if nums[left]+nums[right] > sum {
+				right--
+			} else {
+				left++
+			}
+		}
 	}
 
 	return result
